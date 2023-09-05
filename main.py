cfb90b04d47851c32874fea181cc003dfb8957a6
Jesús Bocanegra <jesusbodc@gmail.com>
2023-09-05 10:49:42 -0500

    Creados los esqueletos de las funciones

---
+++ main.py
@@ -0,0 +1,8 @@
+def generadorDeFuncion(vx: float, vy: float, g: float, h: float):
+    def funcionGenerada(t: float):
+        pass
+    return funcionGenerada
+    
+
+if __name__ == "__main__":
+    funcion = generadorDeFuncion()
