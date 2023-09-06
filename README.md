8cff48d82e30a8ba4ba34f85fc690b21dd7b8508
Jesús Bocanegra <jesusbodc@gmail.com>
2023-09-06 15:52:45 -0500

    Readme actualizado, main establecido

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # PyFreeFall
 
 Proyecto para mostrar el fenómeno del tiro parabólico
+
+##
+
+Esto se basa en el par de ecuaciones
+$$ x = v_xt + x_0 $$
+$$
+y = -gt^2 + v_yt + y_0 
+$$
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+
 def generadorDeFuncion(vx: float, vy: float, g: float, h: float):
+    """Genera una función para describir la posición de un objeto usando las fórmulas del tiro parabólicos
+    
+    Parametros
+    ----------
+    vx : float
+        La velocidad horizontal en (m/s)
+    vy : float
+        La velocidad vertical en (m/s)
+    g : float
+        La aceleración de la gravedad en (m/s^2)
+    h : float
+        La altura en (m)
+    """
+
     def funcionGenerada(t: float):
         pass
+    
     return funcionGenerada
     
 
